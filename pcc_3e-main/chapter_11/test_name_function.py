a7b8d3c6052911a13ea338251628d583e801f63a
Junyoung_Kim <zzz5686@naver.com>
2024-10-01 14:37:55 +0900

    1001 파이썬 커밋

--- pcc_3e-main/chapter_11/test_name_function.py
+++ pcc_3e-main/chapter_11/test_name_function.py
@@ -1,7 +1,7 @@
 from name_function import get_formatted_name
 
-
-def test_first_last_name():
+## 파일 이름이 test_***으로 시작해야 한다.
+def test_first_last_name(): # 테스트 함수도 test_***으로 시작해야 한다.
     """Do names like 'Janis Joplin' work?"""
     formatted_name = get_formatted_name('janis', 'joplin')
     assert formatted_name == 'Janis Joplin'
